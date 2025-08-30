e4ccec62edbdc998bf040083b5c4a9111478c9dd
Reo Nishida <leo.nishida1030@gmail.com>
2025-08-30 18:47:14 +0900

    feat: implement full task CRUD logic and complete task app

--- src/app/(main)/expired/page.tsx
+++ src/app/(main)/expired/page.tsx
@@ -1,6 +1,21 @@
 import TaskCard from "@/components/TaskCard/TaskCard"
+import { TaskDocument } from "@/models/task";
 
-const ExpiredTaskPage = () => {
+const getExpiredTasks = async (): Promise<TaskDocument[]> => {
+  const response = await fetch(`${process.env.API_URL}/tasks/expired`, {
+    cache: "no-store"
+  });
+
+  if(response.status !== 200) {
+    throw new Error();
+  }
+
+  const data = await response.json();
+  return data.tasks as TaskDocument[];
+}
+
+const ExpiredTaskPage = async () => {
+  const expiredTasks = await getExpiredTasks();
   return (
     <div className="text-gray-800 p-8 h-full overflow-auto pb-24">
       <header className="flex justify-between items-center">
@@ -8,7 +23,7 @@ const ExpiredTaskPage = () => {
         
       </header>
       <div className="mt-8 flex flex-wrap gap-4">
-        <TaskCard />
+        {expiredTasks.map((task) => <TaskCard key={task._id} task={task}/>)}
       </div>
     </div>
   )
